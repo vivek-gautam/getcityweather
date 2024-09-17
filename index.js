57d8601a81a94e498e73beeba363c90ad174da73
vivek-gautam <69036935+vivek-gautam@users.noreply.github.com>
2024-09-17 14:51:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded",function(){
             document.querySelector('#temp').innerHTML=`${(temp-273).toFixed(2)} &#176;C` ;
             document.querySelector('#name').innerHTML=`Weather in ${name}`;
             document.querySelector('#desc').innerHTML=`${data['weather'][0]['main']}` ;
-            document.querySelector('.background').style.backgroundImage= `url('https://source.unsplash.com/1480x768/?${name}'`;
-            document.querySelector('.background').style.filter = 'blur(5px)';
+            // document.querySelector('.background').style.backgroundImage= `url('https://source.unsplash.com/1480x768/?${name}'`;
+            // document.querySelector('.background').style.filter = 'blur(5px)';
             document.querySelector('#feels').innerHTML=`Feels like ${(data['main']['feels_like']-273).toFixed(2)}&#176;C`;
             document.querySelector("#min-max").innerHTML=`${(data['main']['temp_min']-273).toFixed(1)}&#176;C / ${(data['main']['temp_max']-273).toFixed(1)}&#176;C`;
             document.querySelector('img').src=`https://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`;
